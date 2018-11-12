455e4426876d23e40effad7f42e8cb337218b08f
srfoster <stephen@thoughtstem.com>
2018-11-12 15:59:17 -0800

    factoring out starter file declarations, much simpler now.  Quest 5 fixes

--- quest5.rkt
+++ quest5.rkt
@@ -1,5 +1,40 @@
 #lang racket
 
+;High level goals:
+#;(
+
+   Title: Score-related GUIs
+
+   Main goal: Add a score GUI
+      * Add a score keeping entity GUI
+
+   Stretch goals:
+      * Add game over screen GUI
+      * Customize game over screen GUI
+
+      
+   ======
+
+   Quest-complete goals:  Create a score keeping entity and game over screen, using less scaffolding.
+      * Suggestions (since it doesn't make sense to add MORE score-related GUIs)
+         * Ask student to: Remove the score-related GUIs from the game without breaking anything else
+         * Add them back in with fewer (no?) quest cards
+
+   Mastery level 1:  I have added a score keeping and game over GUIs to a game enough times that I can
+       do it without the quest cards.
+    * Test your mastery:  Do it in under 5 minutes without hint cards.
+    You may use an existing file as long as it does not have a score keeping entity already.
+
+
+   Mastery level 2:  I understand the functions I'm using, and their documented meanings,
+   well enough to be able to add and customize a new score keeping entity.
+       * Test your mastery:  Using only the documentation, add score-related GUIs
+       to a game that doesn't have them.
+      
+   )
+
+
+
 (provide quest5)
 
 (require ts-racket)
@@ -14,155 +49,134 @@
 
 (require "./common.rkt")
 
-
-(define-webpage score-video
-  this-curriculum
-  "https://bit.ly/2vh0aw0")
-
-(define-webpage copy-paste-video2
-  this-curriculum
-  "https://bit.ly/2HqIEdA")
-
-(define-webpage modify-win-video
-  this-curriculum
-  "https://bit.ly/2HsgDlH")
-
-(define-webpage add-score-video
-  this-curriculum
-  "https://bit.ly/2HlBRCq")
-
-(define-webpage download-video
-  this-curriculum
-  "https://bit.ly/2Hpg60v")
-
-
-(define-webpage resize-video
-  this-curriculum
-  "https://bit.ly/2Jj525O")
-
-(define-webpage insert-background-video
-  this-curriculum
-  "https://bit.ly/2HmmPww")
-
 (define-racket-file keep-score-code
   starter-files
   "tsgd_keep_score.rkt")
 
 (define-launcher-list keep-score
+  paste-the-code-below-into-your-file
   keep-score-code
-  paste-the-code-above-into-your-file)
+  paste-the-code-above-into-your-file
+  between-definitions-explanation)
+
+(define-racket-file game-over-code
+  starter-files
+  "tsgd_game_over_gui.rkt")
+
+(define-launcher-list game-over
+  paste-the-code-below-into-your-file
+  game-over-code
+  paste-the-code-above-into-your-file
+  between-definitions-explanation)
+
+
+(define (start-game-score-code)
+ 
+  (define target (p:frame #:color "red"
+                          (p:code (score-entity))))
+  
+  (define code-img
+    (p:code (start-game (instructions-entity)
+                        ...
+                        #,target
+                        ...
+                        bg-entity)))
+
+  (code+hints code-img
+              (list target (hint "New code"))))
+
+(define-image-file start-game:score
+  images
+  (start-game-score-code))
+
+
+(define (start-game:game-over-code)
+ 
+  (define target (p:frame #:color "red"
+                          (p:code (game-over-screen))))
+  
+  (define code-img
+    (p:code (start-game (instructions-entity)
+                        ...
+                        #,target
+                        ...
+                        bg-entity)))
+
+  (code+hints code-img
+              (list target (hint "New code"))))
+
+(define-image-file start-game:game-over
+  images
+  (start-game:game-over-code))
 
                                                  
-;(with-award 2 modify-score)
+
 (define modify-score
-  (activity-instructions "Modify the Keep Score Code"
+  (activity-instructions "Add Score to start-game"
                          '()
                          (list
-                          (instruction-basic "Change \"hero\" and \"chest\" to match your game.")
-                          (instruction-basic "Find the (start-game) code and add (score-entity) after (game-over-screen).")
+                          (instruction-basic "Find the (start-game) code and add (score-entity).")
                           (instruction-basic "Test the game.")
                           (instruction-goal "your score changing in game."))
-                        (launcher-img score-video)))
+                        (launcher-img start-game:score)))
 
 
 
-;(with-award 1 copy-paste-keep-score) 
-(define copy-paste-keep-score
-  (activity-instructions "Copy and Paste the Code"
-                         '()
-                         (list (instruction-basic "Highlight the new code.")
-                               (instruction-basic "Press CTRL+C to COPY.")
-                               (instruction-basic "Go back to your project and make a new line BEFORE (define lost?)")
-                               (instruction-basic "Press CTRL+V to PASTE.")
-                               (instruction-goal "your new code in your project"))
-                         (launcher-img copy-paste-video2)))
-
-
-(define won-code-img (p:scale (p:code (define (won? g e) 
-                                    (define score (get-counter (get-entity "score" g)))
-                                    (>= score 1000)))
-                            4 ))
-
-(define modify-won 
-  (activity-instructions "Modify the Win Condition"
-                         '()
-                         (list                        
-                          (instruction-basic "Find the '(define (won?' code.")
-                          (instruction-basic "Modify the code to read the counter from score-entity. It should look like this:")
-                          (instruction-image won-code-img 640 100 "")
-                          ;(instruction-basic "Change 'speed' to 'score'.")
-                          ;(instruction-basic "Change 'get-speed' to 'get-counter'.")
-                          ;(instruction-basic "Change 'get-entity \"###\"' to 'get-entity \"score\"'.")
-                          ;(instruction-basic "Change (>= speed 10) to        (>= score 1000).")
-                          (instruction-goal "a score of 1000 in game."))
-                        (launcher-img modify-win-video)))
-
-
-
-(define add-score-background
-  (activity-instructions "Add a Score Background"
-                         '()
-                         (list (instruction-basic "TS Magic Load: 'tsgd_score_bg'.")
-                               (instruction-basic "Copy/Paste the code BEFORE (define (score-entity).")
-                               (instruction-basic "Add (score-bg) BEFORE (score-entity) in (start-game).")
-                               (instruction-goal "your score background in game."))
-                         (launcher-img add-score-video)))
-
-
-
-
-(define download-background
-  (activity-instructions "Download a New Background"
+(define game-over-screen-start-game
+  (activity-instructions "Add to start-game"
                          '()
                          (list
-                          (instruction-basic "Open Google Chrome and click 'CC Search'.")
-                          (instruction-basic "Find a new background image and save it in SAVE_MY_WORK.")
-                          (instruction-goal "your image saved in the folder."))
-                         (launcher-img download-video)))
-
-
-
-(define open-resize-pinta
-  (activity-instructions "Resize the Background"
-                         '()
-                         (list
-                          (instruction-open "Pinta")
-                          (instruction-basic "Open the downloaded image.")
-                          (instruction-basic "Resize and crop the image to 640 by 480.")
-                          (instruction-goal "your resized image in Pinta."))
-                         (launcher-img resize-video)))
-
-
-
-(define save-insert-background
-  (activity-instructions "Save & Insert Background"
-                         '()
-                         (list
-                          (instruction-basic "In Pinta, rename and save the image to SAVE_MY_WORK.")
-                          (instruction-basic "In DrRacket, delete the old bg and insert your new one.")
+                          (instruction-basic "Add (game-over-screen) to (start-game ...)")
+                          (instruction-basic "Use the launch code to see how")
                           (instruction-basic "Test your game.")
-                          (instruction-goal "your new background in-game."))
-                         (launcher-img insert-background-video)))
+                          (instruction-goal "your game over screen in your game"))
+                         (launcher-img start-game:game-over)))
+
+
+(define customize-game-over-message
+  (activity-instructions "Edit the game over message"
+                         '()
+                         (list
+                          (instruction-basic "Change the game over message.")
+                          (instruction-basic "HINT: Look inside the (game-over-screen) definition.")
+                          (instruction-goal "your customized message in your game"))
+                         (p:scale
+                          (p:bitmap (build-path images "game-over-screen-example.png"))
+                          0.25)))
+
+
+
+
+(define customize-game-over-message-more
+  (activity-instructions "Edit the game over message"
+                         '()
+                         (list
+                          (instruction-basic "Design your own game over image in Piskel and export")
+                          (instruction-basic "Replace the game over image in your code.")
+                          (instruction-goal "your updated game over screen in your game"))
+                         (p:scale (logo-for "piskel") 4)
+                         ))
 
 ;Add a Score Entity
 (define day5-2dgame
   (list
    (with-award 0 open-file)
-   (with-award 0 (load-new-code keep-score))
-   (with-award 1 copy-paste-keep-score)       
-   (with-award 2 modify-score)                
-   (with-award 2 modify-won)
+   (with-award 0 (load-new-code keep-score))      
+   (with-award 2 modify-score)
+
    (choose "any"
-          (list
-           (with-award 1 add-score-background)
-           (with-award 1 download-background)
-           (with-award 2 open-resize-pinta)
-           (with-award 2 save-insert-background) 
-           ))
+           (list
+            (with-award 1 (load-new-code game-over))
+            (with-award 1 game-over-screen-start-game)
+            (with-award 1 customize-game-over-message)
+            
+            (with-award 1 customize-game-over-message-more)
+            ))
    ))
 
 (define (quest5)
-  (map shrink (make-picts "red" "Q3-" day5-2dgame (settings (bg (local-bitmap "bg-arcade.png")) MARIO MARIO-BONUS MARIO-BONUS))))
+  (map shrink (make-picts "red" "Q5-" day5-2dgame (settings (bg (local-bitmap "bg-arcade.png")) R-MUSH R-MUSH-BONUS R-MUSH-BONUS))))
 
 
-
+(module+ test
+  (quest5))
